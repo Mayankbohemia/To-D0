2df0e5ed47459943c55d1b98001834647b1a79ba
Mayankbohemia <b160024@nitsikkim.ac.in>
2023-11-13 10:59:04 +0530

    change

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('hello world')
+const showModal = document.querySelector('.btn__add');
+const taskModal = document.querySelector('.task__modal ');
+const addForm = document.querySelector('.add__form');
+const error = document.querySelector('.error');
+const backdrop = document.querySelector('.backdrop');
+
+
+const taskArr = []
+showModal.addEventListener('click', function () {
+    taskModal.classList.add('show__modal');
+    backdrop.classList.add('blur')
+})
+
+backdrop.addEventListener('click', function() {
+    taskModal.classList.remove('show__modal');
+    backdrop.classList.remove('blur')
+})
+
+
+
+// ------------------------------Validation-------------------------------------------
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const task = document.forms['addForm']['task']
+    if (task.value.trim() !== '') {
+        taskModal.classList.remove('show__modal')
+        backdrop.classList.remove('blur')
+    }
+    if (task.value.trim() === '') {
+        error.innerHTML = '*enter valid task!'
+
+    }
+    task.addEventListener('focus', function() {
+        error.innerHTML = ''
+
+    })
+
+});
+
+
+
